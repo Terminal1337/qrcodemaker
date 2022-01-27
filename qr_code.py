0b2412e05cb9e647858a8bc6fa65283f23e034aa
Terminal1337 <95563109+Terminal1337@users.noreply.github.com>
2022-01-27 01:13:42 -0800

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -10,7 +10,7 @@ print(Fore.GREEN + '''
 print(Fore.GREEN + "                                                                    -Terminal1337")
 
 import os
-Condition = input("Do you want me to install required module for you? Y/n: ")
+Condition = input(Fore.BLUE + "[+] Do you want me to install required module for you? Y/n: ")
 if Condition == "Y":
     os.system("pip install qrcode")
     pass
@@ -19,15 +19,15 @@ elif Condition == "n":
 else:
     print("Error: Invalid Input")
 import qrcode
-path = input("Enter The URL/TEXT to conver to QR: ")
-output = input("Enter the Name of Output File: ")
+path = input(Fore.RED + "[+] Enter The URL/TEXT to conver to QR: ")
+output = input(Fore.LIGHTCYAN_EX + "[+] Enter the Name of Output File: ")
 def qr():
     image = qrcode.make(path)
     image.save(output+'.png')
 
 qr()
-print("QR Code has been successfully written to",output)
-lola = input("do you want to close the program(Y/n): ")
+print(Fore.RED + "[-] QR Code has been successfully written to",output)
+lola = input(Fore.MAGENTA + "[+] do you want to close the program(Y/n): ")
 if lola == "Y":
     sys.exit()
 else:
